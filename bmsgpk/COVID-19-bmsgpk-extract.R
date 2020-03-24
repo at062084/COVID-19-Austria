677960210868472cf4e7835a28553bf65ec7b0de
Thomas Strehl <Thomas.Strehl@at.ibm.com>
2020-03-24 10:44:56 +0100

    Make data extraction from bmsgpk site more capable to handle nonstandard reports. Add logfile

--- bmsgpk/COVID-19-bmsgpk-extract.R
+++ bmsgpk/COVID-19-bmsgpk-extract.R
@@ -3,9 +3,17 @@ library(lubridate)
 library(stringr)
 library(readr)
 library(dplyr)
+options(error = function() traceback(2))
 
 # set work dir here
 setwd("/home/at062084/DataEngineering/COVID-19/COVID-19-Austria/bmsgpk")
+
+# do some logging
+logFile <- "./html/COVID-19.extract.log"
+logMsg <- function(msg) {
+  cat(paste(format(Sys.time(), "%Y%m%d-%H%M%OS3"), msg, "\n"), sep="", file=logFile, append=TRUE)
+  cat(paste(format(Sys.time(), "%Y%m%d-%H%M%OS3"), msg, "\n"), sep="")
+}
 
 #read manually maintained excel sheet
 #library(readxl)
@@ -32,15 +40,16 @@ dR=nRows+4
 
 # Identifiers of sections  in html response of wget 
 atTested <- "<p><strong>Bisher durchgef&uuml;hrte Testungen in &Ouml;sterreich"
-atConfirmed <- "<p><strong>Best&auml;tigte F&auml;lle"
-atRecovered <- "<p><strong>Genesene Personen"
-atDeaths <- "<p><strong>Todesf&auml;lle"
+atConfirmed <- "<p><strong>Best&auml;tigte F&auml;lle, </strong><strong>Stand"
+atRecovered <- "<p><strong>Genesene Personen,</strong> <strong>Stand "
+atDeaths <- "<p><strong>Todesf&auml;lle</strong>, <strong>Stand"
 closeAll <- "</p>"
 
 # Stamp
 s <- str_extract(html, paste0(atConfirmed,".*",closeAll))
 t <- stringr::str_match(s,paste0("<strong>Stand ","([\\d\\s\\.,:]*)"," Uhr:</strong>"))[2]
 Stamp <- as.POSIXct(t, format="%d.%m.%Y, %H:%M", tz="CET")
+logMsg(paste("Running extraction for", Stamp, "from", bmsgpkFile))
 
 # Extract total number of Tested
 s <- str_extract(html, paste0(atTested,".*",closeAll))
@@ -58,40 +67,54 @@ Bundeslaender <- data.frame(Name=c("Burgenland","K&auml;rnten","Nieder&ouml;ster
 s <- str_extract(html, paste0(atConfirmed,".*",closeAll))
 df[cR,"Stamp"] <- Stamp 
 df[cR,"Status"] <- "Confirmed"
-for (bl in Bundeslaender$Name) {
-  n <- as.integer(str_match(s,paste0(bl," \\(","(\\d*)","\\)"))[2])
-  #cat(bl,n,"\n")
-  df[cR,Bundeslaender[Bundeslaender$Name==bl,2]] <-n
+if(!is.na(s)) {
+  nAT <- as.integer(str_remove(str_match(s,paste0("Uhr:</strong>&nbsp;","([\\d\\.]*)"," F&auml;lle,"))[2],"\\."))
+  df[cR,"AT"] <- nAT
+  for (bl in Bundeslaender$Name) {
+    n <- as.integer(str_remove(str_match(s,paste0(bl," \\(","([\\d\\.]*)","\\)"))[2],"\\."))
+    #cat(bl,n,"\n")
+    df[cR,Bundeslaender[Bundeslaender$Name==bl,2]] <-n
+  }
+  #df[cR,"AT"] <- sum(df[cR,c(Bundeslaender[,2])])
+} else {
+  logMsg("WARN: No record for Confirmed found")
 }
-df[cR,"AT"] <- sum(df[cR,c(Bundeslaender[,2])])
 
 # Extract number of Recovered cases
 s <- str_extract(html, paste0(atRecovered,".*",closeAll))
-nAT <- as.numeric(str_match(s,paste0(":</strong> (\\d*),"))[2])
 df[rR,"Stamp"] <- Stamp 
 df[rR,"Status"] <- "Recovered"
-df[rR,"AT"] <- nAT
-for (bl in Bundeslaender$Name) {
-  n <- str_match(s,paste0(bl," \\(","(\\d*)","\\)"))[2]
-  if (is.na(n)) n <- 0
-  df[rR,Bundeslaender[Bundeslaender$Name==bl,2]] <-n
-  #cat(bl,n,"\n")
+if (!is.na(s)) {
+  nAT <- as.integer(str_remove(str_match(s,paste0(":</strong> (","([\\d\\.]*)","\\)"))[2],"\\."))
+  df[rR,"AT"] <- nAT
+  for (bl in Bundeslaender$Name) {
+    n <- as.integer(str_remove(str_match(s,paste0(bl," \\(","([\\d\\.]*)","\\)"))[2],"\\."))
+    if (is.na(n)) n <- 0
+    df[rR,Bundeslaender[Bundeslaender$Name==bl,2]] <-n
+    #cat(bl,n,"\n")
+  }
+} else {
+  logMsg("WARN: No record for Recovered found")
 }
 
 # Extract number of Deaths
 s <- str_extract(html, paste0(atDeaths,".*",closeAll))
-nAT <- as.numeric(str_match(s,paste0("Uhr:</strong> (\\d*),"))[2])
 df[dR,"Stamp"] <- Stamp 
 df[dR,"Status"] <- "Deaths"
-df[dR,"AT"] <- nAT
-for (bl in Bundeslaender$Name) {
-  n <- str_match(s,paste0(bl," \\(","(\\d*)","\\)"))[2]
-  if (is.na(n)) n <- 0
-  df[dR,Bundeslaender[Bundeslaender$Name==bl,2]] <-n
-  #cat(bl,n,"\n")
+sAT <- str_match(s,paste0("Uhr:</strong> ","([\\d\\.]*)",", nach"))[2]
+if (!is.na(sAT)) {
+  nAT <- as.integer(str_remove(sAT,"\\."))
+  df[dR,"AT"] <- nAT
+  for (bl in Bundeslaender$Name) {
+    n <- n <- as.integer(str_remove(str_match(s,paste0(bl," \\(","([\\d\\.]*)","\\)"))[2],"\\."))
+    if (is.na(n)) n <- 0
+    df[dR,Bundeslaender[Bundeslaender$Name==bl,2]] <-n
+    #cat(bl,n,"\n")
+  }
+  # df[dR,"AT"] <- sum(df[dR,c(Bundeslaender[,2])])
+} else {
+  logMsg("WARN: No record for Desaths found")
 }
-
-tail(df)
 
 # Persist data
 write.csv(df, csvFile, row.names=FALSE, quote=FALSE)
