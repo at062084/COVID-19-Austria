36b6b5873a5d48eb361dc0a105070c87a25af4e9
Thomas Strehl <Thomas.Strehl@at.ibm.com>
2020-03-29 16:32:59 +0200

    Move covCounty extractor into own script. Add conCounty to chron script. Correct datestamp in cov-county format

--- COVID-19-cron.sh
+++ COVID-19-cron.sh
@@ -24,14 +24,34 @@ if [ "$SECTION" == "bmsgpk" ]
 then
 	# Run Rscript that downlads and stores the data from bmsgpk
 	cd  $BASE_DIR/bmsgpk
+
 	echo "$STAMP Running auto-extraction of COVID-19 data from bmsgpk website"  >> $LOG_FILE
 	/usr/bin/Rscript ./COVID-19-bmsgpk-extract.R 2>&1 >> $LOG_FILE
-	git add $LOG_FILE
+
+	# Update git
+	git add ../*.log
+	git add *.log
 	git add ./html/*
 	git add ./data/*
 	git commit -m "$STAMP Auto-Extract data from bmsgpk website" 2>&1 >> $LOG_FILE
 	git push 2>&1 >> $LOG_FILE
 fi
+
+if [ "$SECTION" == "county" ]
+then
+	# Run Rscript that downlads and stores the data from bmsgpk
+	cd  $BASE_DIR/bmsgpk
+
+	echo "$STAMP Running conversion of COVID-19 data to covid-county format"  >> $LOG_FILE
+	/usr/bin/Rscript ./COVID-19-covid-county-extract.R 2>&1 >> $LOG_FILE
+
+	# Update git
+	git add ../*.log
+	git add *.log
+	git add ./data/*
+	git commit -m "$STAMP Auto-Conversion of bmsgpk data to covid-county format" 2>&1 >> $LOG_FILE
+	git push 2>&1 >> $LOG_FILE
+
 
 if [ "$SECTION" == "detail" ]
 then
